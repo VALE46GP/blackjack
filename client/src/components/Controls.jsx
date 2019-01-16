73c07e2398c93616f7c8a59d1bceaf8aac3a70fb
Eric Callari <ericcallari1@gmail.com>
2019-01-16 13:57:44 -0800

    add totals to display

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -7,6 +7,8 @@ class Controls extends React.Component {
     this.state = {
       yourHand: {
         bets: [0],
+        totals: [[]],
+        displays: [],
       },
     };
   }
@@ -29,13 +31,22 @@ class Controls extends React.Component {
       hit, stay, doubledown, state,
     } = this.props;
     const { yourHand } = state;
+    const display = (total) => {
+      if (typeof total === 'string') {
+        return total;
+      }
+      if (total[1] !== total[0] && total[1] <= 21) {
+        return `Total: ${total[0]} / ${total[1]}`;
+      }
+      return `Total: ${total[0]}`;
+    }
     return (
       <div className="controls-container">
         <div className="hit active" onClick={() => hit()} onKeyPress={() => hit()} role="button" tabIndex={0}>HIT</div>
         <div className="stay active" onClick={() => setTimeout(() => { stay(); }, 500)} onKeyPress={() => setTimeout(() => { stay(); }, 500)} role="button" tabIndex={0}>STAY</div>
         <div className="double active" onClick={() => doubledown()} onKeyPress={() => doubledown()} role="button" tabIndex={0}>DOUBLE</div>
         <div className="split inactive" onClick={() => doubledown()} onKeyPress={() => doubledown()} role="button" tabIndex={0}>SPLIT</div>
-        <div className="bet-info">{yourHand.bets[0]}</div>
+        <div className="bet-info">{display(yourHand.totals[yourHand.turn])}</div>
       </div>
     );
   }
